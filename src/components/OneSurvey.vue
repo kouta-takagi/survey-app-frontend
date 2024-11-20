3fc639f65d29ff93b268ebde458c7bb74d5f2c19
kouta-takagi <k-takagi@ruby-dev.jp>
2024-11-20 13:42:06 +0900

    微修正

--- src/components/OneSurvey.vue
+++ src/components/OneSurvey.vue
@@ -3,6 +3,10 @@ import { inject, ref } from "vue";
 import type { Survey } from "../types/survey";
 import Questions from "./Questions.vue";
 import { RouterLink } from "vue-router";
+import axios from "axios";
+import { useMessage } from "@/composables/useMessage";
+
+const { message } = useMessage();
 
 interface Props {
   id: number;
@@ -21,6 +25,23 @@ const survey = surveys.get(props.id) as Survey;
 function handleToggle() {
   isOpen.value = !isOpen.value;
 }
+
+function handleDelete() {
+  axios
+    .delete(`http://localhost:3000/surveys/${props.id}`, {
+      headers: {
+        "access-token": localStorage.getItem("access-token"),
+        client: localStorage.getItem("client"),
+        uid: localStorage.getItem("uid"),
+      },
+    })
+    .then(() => {
+      message("アンケートの削除が完了しました", {
+        autoHide: true,
+        hideTime: 3000,
+      });
+    });
+}
 </script>
 
 <template>
@@ -36,6 +57,7 @@ function handleToggle() {
     >
       このアンケートのURL
     </RouterLink>
+    <button v-on:click="handleDelete">このアンケートを削除する</button>
 
     <v-card-text class="bg-surface-light pt-4">
       <div class="d-flex justify-space-between">
